d994fc68f18eaf147c99e17a9baa106a16fd3c34
ANIKETROY11005 <draxnme12345@gmail.com>
2023-03-13 03:07:05 +0530

    first commit added expenses module added it to git hub

---
+++ src/components/NewExpenses.jsx
@@ -0,0 +1,19 @@
+import ExpenseItem from "./ExpenseItem";
+import './NewExpenses.css'
+function NewExpenses(props) {
+
+
+    return (
+        <div className='expenses'>
+            <ExpenseItem title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date} />
+            <ExpenseItem title={props.expenses[1].title} amount={props.expenses[1].amount} date={props.expenses[1].date} />
+            <ExpenseItem title={props.expenses[2].title} amount={props.expenses[2].amount} date={props.expenses[2].date} />
+            <ExpenseItem title={props.expenses[3].title} amount={props.expenses[3].amount} date={props.expenses[3].date} />
+        </div>
+
+    );
+
+
+};
+
+export default NewExpenses;
