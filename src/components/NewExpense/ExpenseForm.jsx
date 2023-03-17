262a7b9d1f2436e15d417ca7adb599ee696bbee0
ANIKETROY11005 <draxnme12345@gmail.com>
2023-03-18 05:14:13 +0530

    made  add expense form to be rendered conditionally

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -53,6 +53,13 @@ const ExpenseForm = (props) => {
         setEnteredDate('');
     };
 
+    // const cancelHandler = () => {
+    //     const isCancel = true
+    //     props.onCancel(isCancel);
+    // }
+
+
+
     return (
         <form onSubmit={submitHandler} >
             <div className='new-expense__controls'>
@@ -70,8 +77,10 @@ const ExpenseForm = (props) => {
                 </div>
             </div>
             <div className='new-expense__actions'>
-                <button type='submit'>Add Expense</button>
+                <button type='button' onClick={props.onCancel}>Cancel</button>
+                <button type='submit' >Add Expense</button>
             </div>
+
         </form>
     )
 }
--- src/components/NewExpense/NewExpense.jsx
+++ src/components/NewExpense/NewExpense.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import ExpenseForm from './ExpenseForm';
 
 import './NewExpense.css';
@@ -9,12 +10,29 @@ const NewExpense = (props) => {
             id: Math.random().toString()
         };
         props.onAddExpense(expenseData);
+        setOpenForm(false);
     };
+
+    const [openForm, setOpenForm] = useState(false);
+    const clickHandler = () => {
+        setOpenForm(true);
+    }
+
+    const onCancelHandler = () => {
+        setOpenForm(false);
+    }
+
+    let form = <button className='new-expense__button' onClick={clickHandler}>Add New Expense</button>
+    if (openForm) {
+        form = <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler} onCancel={onCancelHandler} />
+    }
 
 
     return (
         <div className='new-expense'>
-            <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler} />
+
+            {form}
+
         </div>
     )
 };
