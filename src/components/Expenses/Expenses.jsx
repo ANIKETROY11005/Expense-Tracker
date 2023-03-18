eb0b7aaa10bebcf2bbc1c608a61eb4b280850c50
ANIKETROY11005 <draxnme12345@gmail.com>
2023-03-18 08:47:15 +0530

    added the Char component to the app

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 import './Expenses.css';
 const Expenses = (props) => {
 
@@ -28,6 +29,7 @@ const Expenses = (props) => {
 
             <Card className='expenses'>
                 <ExpensesFilter selectValue={FilterDate} onChangedDate={changedDateHandler} />
+                <ExpensesChart expenses={filteredExpenses} />
                 <ExpensesList items={filteredExpenses} />
 
             </Card>
